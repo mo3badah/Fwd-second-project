dcd31cb973983918c283d914eb90d6ffc5904e70
Mohamed Reda <alprofmohamed123@gmail.com>
2020-12-12 00:36:25 +0200

    there is an error

--- shapes3d/ball.js
+++ shapes3d/ball.js
@@ -5,4 +5,4 @@ bl.h=bl.w
 console.log(`the 3D shape is : ${bl.sh} and its properties is : ${bl.w} , ${bl.h} , ${bl.h} 
 It's area is : ${bl.a} 
 It's length is : ${bl.l} 
-It's volume is : ${bl.v} `)
+It's volume is : ${bl.v}`)
--- shapes3d/shape.js
+++ shapes3d/shape.js
@@ -1,15 +1,15 @@
-let sh,w,h,a,l,v;
+//let sh,w,h,a,l,v;
 module.exports = {
     "sh":"",
     "w":"",
     "h":"",
-    "a":w*h,
-    "l":(w+h)*2,
-    "v":a*h,
-    "prt":console.log(`the 3D shape is : ${sh} and its properties is : ${w} , ${w} , ${h} 
-It's area is : ${a} 
-It's length is : ${a} 
-It's volume is : ${a} 
+    "a":this.w*this.h,
+    "l":(this.w+this.h)*2,
+    "v":this.a*this.h,
+    "prt":console.log(`the 3D shape is : ${this.sh} and its properties is : ${this.w} , ${this.h} , ${this.h} 
+It's area is : ${this.a} 
+It's length is : ${this.l} 
+It's volume is : ${this.v} 
  `)
 }
 
